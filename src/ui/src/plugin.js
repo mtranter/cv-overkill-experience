b38f63639ac8743f35030d34c420977a7db5098a
Mark Tranter <mark.a.tranter@gmail.com>
2017-05-08 16:42:47 +0100

    Tweek plugin

--- src/ui/src/plugin.js
+++ src/ui/src/plugin.js
@@ -1,6 +1,6 @@
 export function configure(config){
   let container = config.container;
   container.registerInstance('plugin.route',  { route: ['experience'],       name: 'experience',       moduleId: 'experience/index' })
-  container.registerInstance('plugin.widget.homepage.component', {viewModel: 'experience', view:'experience/experience.html'});
-  config.globalResources('./experience')
+  container.registerInstance('plugin.widget.homepage.component', {viewModel: 'experience/experience', view:'experience/experience.html'});
+  //config.globalResources('./experience')
 }
