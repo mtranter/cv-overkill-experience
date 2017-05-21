6cf0c923e9e1c07eac1e7bc29d446b954da5659e
Mark Tranter <mark.a.tranter@gmail.com>
2017-05-21 19:21:22 +0100

    Add region

--- deploy/module.tf
+++ deploy/module.tf
@@ -12,6 +12,10 @@ variable "region" {
   default = "eu-west-1"
 }
 
+provider "aws" {
+  region = "eu-west-1"
+}
+
 module "experience" {
   source = "github.com/mtranter/cv-overkill-terraform?ref=v1.0//modules/tf-cv-overkill-aurelia-module"
   website_files = ["app-bundle.js"]
