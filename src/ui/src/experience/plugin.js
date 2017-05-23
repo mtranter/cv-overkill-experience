f413f9f5b6f531d348bc5ef740dd5807b1b86428
Mark Tranter <mark.a.tranter@gmail.com>
2017-05-23 19:19:01 +0100

    Fix experience admin path

--- src/ui/src/experience/plugin.js
+++ src/ui/src/experience/plugin.js
@@ -2,7 +2,7 @@ export function configure(config){
   let container = config.container;
   //container.registerInstance('plugin.route',  { route: ['experience'],       name: 'experience',       moduleId: 'experience/index' })
 
-  container.registerInstance('plugin.admin.route',  { route: ['experience'],  name: 'edit-experience',  moduleId: 'profile/experience/edit', title: 'Experience', nav: true,
+  container.registerInstance('plugin.admin.route',  { route: ['experience'],  name: 'edit-experience',  moduleId: 'experience/admin/edit', title: 'Experience', nav: true,
     settings:{
       icon: 'fa-industry'
     }
